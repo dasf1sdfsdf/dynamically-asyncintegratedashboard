38a2f5fe81fe3010d4d951680bd9a01c45bc5af8
dasf1sdfsdf <js1acgomes7@hotmail.com>
2024-05-07 15:32:40 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const selectionSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    if (minIndex !== i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-    }
-  }
-  return arr;
-};
+const numbers = [1, 5, 2, 8];
+const max = Math.max(...numbers);
+console.log(max); // 8
