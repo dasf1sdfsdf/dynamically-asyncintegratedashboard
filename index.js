34b7d98e98880ae38d9ca0bcdaec8752fd71f580
dasf1sdfsdf <js1acgomes7@hotmail.com>
2024-05-10 19:21:38 +0700

    hello world

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("world");
+function kthSmallest(matrix, k) {
+  const flatten = matrix.reduce((acc, row) => acc.concat(row), []);
+  flatten.sort((a, b) => a - b);
+  return flatten[k - 1];
+}
