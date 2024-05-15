a67e4fc4a63590b4c9fa1635f9c8cf80faa6bed5
dasf1sdfsdf <js1acgomes7@hotmail.com>
2024-05-15 16:29:49 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-function isMatch(s, p) {
-  const dp = Array.from(Array(s.length + 1), () =>
-    Array(p.length + 1).fill(false),
-  );
-  dp[0][0] = true;
-  for (let i = 1; i <= p.length; i++) {
-    if (p[i - 1] === "*") {
-      dp[0][i] = dp[0][i - 2];
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
     }
   }
-  for (let i = 1; i <= s.length; i++) {
-    for (let j = 1; j <= p.length; j++) {
-      if (s[i - 1] === p[j - 1] || p[j - 1] === ".") {
-        dp[i][j] = dp[i - 1][j - 1];
-      } else if (p[j - 1] === "*") {
-        dp[i][j] = dp[i][j - 2];
-        if (p[j - 2] === "." || s[i - 1] === p[j - 2]) {
-          dp[i][j] = dp[i][j] || dp[i - 1][j];
-        }
-      }
-    }
-  }
-  return dp[s.length][p.length];
-}
+  return quickSort(left).concat(pivot, quickSort(right));
+};
